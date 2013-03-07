e1f909e08eaa9df5a8f4361c111c455b78729a06
Jesse Dailey <jesse.dailey@gmail.com>
2013-03-07 16:21:42 -0500

    expand the basic driver to be closer to our Driver Spec

--- conductrics.php
+++ conductrics.php
@@ -9,6 +9,36 @@ class Conductrics {
 class ConductricsAgent {
 	function __construct($name) {
 		$this->name = $name;
+		$this->_baseUrl = null;
+		$this->_apiKey = null;
+		$this->_ownerCode = null;
+	}
+	public function baseUrl($value = null) {
+		if( $value == null ) {
+			if( $this->_baseUrl == null )
+				return Conductrics::$baseUrl;
+			return $this->_baseUrl;
+		}
+		$this->_baseUrl = $value;
+		return $this;
+	}
+	public function ownerCode($value = null) {
+		if( $value == null ) {
+			if( $this->_ownerCode == null )
+				return Conductrics::$ownerCode;
+			return $this->_ownerCode;
+		}
+		$this->_ownerCode = $value;
+		return $this;
+	}
+	public function apiKey($value = null) {
+		if( $value == null ) {
+			if( $this->_apiKey == null )
+				return Conductrics::$apiKey;
+			return $this->_apiKey;
+		}
+		$this->_apiKey = $value;
+		return $this;
 	}
 	private function request($url, $params) {
 		if( count($params) > 0 ) {
@@ -22,25 +52,46 @@ class ConductricsAgent {
 	public function decide($session /*, choices... */) {
 		$choices = array_slice(func_get_args(), 1);
 		$num_choices = count($choices);
-		$url = Conductrics::$baseUrl . "/"
-			. Conductrics::$ownerCode . "/"
-			. $this->name . "/decision/" . $num_choices;
-		$decision = $this->request($url, array(
-			"session" => $session,
-			"apikey" => Conductrics::$apiKey
+		$url = implode("/", array(
+			$this->baseUrl(), $this->ownerCode(), $this->name,
+			"decision", $num_choices
 		));
-		return $choices[$decision->decision % $num_choices];
+		try { return $choices[
+			$this->request($url,
+				array(
+					"session" => $session,
+					"apikey" => $this->apiKey(),
+					"nocache" => rand()
+				)
+			)->decision ];
+		} catch( Exception $err ) {
+			return $choices[0];
+		}
 	}
 	public function reward($session, $goalCode = "goal-1", $value = 1.0) {
-		$url = Conductrics::$baseUrl . "/"
-			. Conductrics::$ownerCode . "/"
-			. $this->name . "/goal/" . $goalCode;
+		$url = implode("/", array(
+			$this->baseUrl(), $this->ownerCode(), $this->name,
+			"goal", $goalCode
+		));
 		return $this->request($url, array(
 			"reward" => $value,
 			"session" => $session,
-			"apikey" => Conductrics::$apiKey
+			"apikey" => $this->apiKey(),
+			"nocache" => rand()
 		));
 	}
+	public function expire($session) {
+		$url = implode("/", array(
+			$this->baseUrl(), $this->ownerCode(), $this->name,
+			"expire"
+		));
+		return $this->request($url, array(
+			"session" => $session,
+			"apikey" => $this->apiKey(),
+			"nocache" => rand()
+		));
+	}
+
 }
 
 ?>
--- demo.php
+++ demo.php
@@ -1,15 +1,42 @@
 <?php require_once "conductrics.php"; ?>
 <?php
+	// Give this visitor a php session if they don't already have one.
+	session_start();
+
+	// If you are a single user, you can set your identity globally.
 	Conductrics::$apiKey = 'api-HFrPvhjnhVufRXtCGOIzejSW';
 	Conductrics::$ownerCode = 'owner_HJJnKxAdm';
 
+	// Create a new agent.
 	$agent = new ConductricsAgent("php-agent");
 
-	$session = "12345";
+	// If you are in a "multi-tenant" scenario (like a WordPress plugin),
+	// You can set your identity per Agent instance.
+	$agent
+		->apiKey('api-HFrPvhjnhVufRXtCGOIzejSW')
+		->ownerCode('owner_HJJnKxAdm');
 
-	echo $agent->decide($session,"a","b");
+?>
 
-	echo json_encode($agent->reward($session));
+<? if( empty($_GET) ): ?>
+	<? // Make a decision about what content to display.
+	if( $agent->decide(session_id(), "a","b") == "a" ): ?>
+		<div><span>The A content was chosen for session <?= session_id() ?>.</span></div>
+	<? else: ?>
+		<div><span>The B content was chosen for session <?= session_id() ?>.</span></div>
+	<? endif; ?>
+	<form action="" method="GET">
+		<input type="hidden" name="value" value="1.2" />
+		<button>Reward</button>
+	</form>
+<? else:
+	// If the button was clicked, send a reward to the Agent.  ?>
+	<?= json_encode($agent->reward(session_id(), "goal-1", $_GET["value"])) ?>
+<? endif; ?>
 
+<?php
+	// Flip a coin to get a new session.
+	if( rand(0,100) < 50 )
+		echo "Ending session " . session_regenerate_id(true);
 ?>
 
